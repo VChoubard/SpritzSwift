b1feed32610ce9218f1a56ed6c70293b3d35cc27
Vincent Choubard <vincentchoubard@gmail.com>
2018-03-26 01:30:48 +0200

    clean up the structure to use carthage

---
+++ SpritzSwiftDemo/Carthage/Build/iOS/SwiftSpritz.framework/Headers/SwiftSpritz.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSpritz.h
+//  SwiftSpritz
+//
+//  Created by vincent Choubard on 26/03/2018.
+//  Copyright © 2018 Vincent Choubard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSpritz.
+FOUNDATION_EXPORT double SwiftSpritzVersionNumber;
+
+//! Project version string for SwiftSpritz.
+FOUNDATION_EXPORT const unsigned char SwiftSpritzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSpritz/PublicHeader.h>
+
+
