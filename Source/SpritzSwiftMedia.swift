69633181b0a86f6968582e7d0742395cb17b59f6
Vincent Choubard <vincentchoubard@gmail.com>
2018-04-02 18:00:26 +0200

    clean xcode project

---
+++ Source/SpritzSwiftMedia.swift
@@ -0,0 +1,13 @@
+//
+//  SpritzSwiftMedia.swift
+//  SpritzSwift
+//
+//  Created by vincent Choubard on 02/04/2018.
+//  Copyright © 2018 Vincent Choubard. All rights reserved.
+//
+
+import UIKit
+
+class SpritzSwiftMedia {
+    
+}
